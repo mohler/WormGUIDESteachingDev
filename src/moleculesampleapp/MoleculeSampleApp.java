2fc9ed76518a4fe7e0f825ab688feb21edbc7dad
wmohler <wmohler@cytoshow.cam.uchc.edu>
2022-02-12 09:38:47 -0500

    Really good experimentation with XformBox behavior.  Ready to fix damn WormGUIDES UI!

--- src/moleculesampleapp/MoleculeSampleApp.java
+++ src/moleculesampleapp/MoleculeSampleApp.java
@@ -18,7 +18,10 @@ public class MoleculeSampleApp extends Application {
     Group root = new Group();
     XformBox cameraXform = new XformBox();
     XformBox ballXForm = new XformBox();
-    Shape3D ball;
+    XformBox ball2XForm = new XformBox();
+    XformBox ball3XForm = new XformBox();
+    XformBox ball4XForm = new XformBox();
+    Shape3D ball, ball2, ball3, ball4;
     PhongMaterial redMaterial, greenMaterial, blueMaterial;
 
     PerspectiveCamera camera = new PerspectiveCamera(true);
@@ -36,7 +39,7 @@ public class MoleculeSampleApp extends Application {
     double mousePosX, mousePosY;
     double mouseOldX, mouseOldY;
     double mouseDeltaX, mouseDeltaY;
-	private Sphere ball2;
+	
 
     private void handleMouse(Scene scene) {
         System.out.printf("handleMouse%n");
@@ -61,12 +64,19 @@ public class MoleculeSampleApp extends Application {
             if (me.isPrimaryButtonDown()) {
                 ballXForm.addRotation(-mouseDeltaX * MOUSE_SPEED * ROTATION_SPEED, Rotate.Y_AXIS);
                 ballXForm.addRotation(mouseDeltaY * MOUSE_SPEED * ROTATION_SPEED, Rotate.X_AXIS);
+                ball2XForm.addRotation(mouseDeltaX * MOUSE_SPEED * ROTATION_SPEED, Rotate.Y_AXIS);
+                ball2XForm.addRotation(mouseDeltaY * MOUSE_SPEED * ROTATION_SPEED, Rotate.X_AXIS);
+                ball3XForm.addRotation(-mouseDeltaX * MOUSE_SPEED * ROTATION_SPEED, Rotate.Y_AXIS);
+                ball3XForm.addRotation(-mouseDeltaY * MOUSE_SPEED * ROTATION_SPEED, Rotate.X_AXIS);
+                ball4XForm.addRotation(mouseDeltaX * MOUSE_SPEED * ROTATION_SPEED, Rotate.Y_AXIS);
+                cameraXform.addRotation(mouseDeltaX * MOUSE_SPEED * ROTATION_SPEED/2, Rotate.X_AXIS);
+                cameraXform.addRotation(-mouseDeltaY * MOUSE_SPEED * ROTATION_SPEED/2, Rotate.Y_AXIS);
             }
         });
     }
 
     private void handleKeyboard(Scene scene) {
-        scene.setOnKeyPressed(event -> ballXForm.reset());
+        scene.setOnKeyPressed(event -> {ballXForm.reset();ball2XForm.reset();ball3XForm.reset();ball4XForm.reset();});
     }
 
     PhongMaterial createMaterial(Color diffuseColor, Color specularColor) {
@@ -107,12 +117,30 @@ public class MoleculeSampleApp extends Application {
         // Build shiney red ball
         ball = new Sphere(50);
         ball.setDrawMode(DrawMode.LINE); // draw mesh so we can watch how it rotates
+        ballXForm.setLayoutX(-50.0);
+        ballXForm.setLayoutY(-50.0);
+        ball.setScaleX(-1);
+        ballXForm.getChildren().add(ball);
         ball2 = new Sphere(50);
         ball2.setDrawMode(DrawMode.LINE); // draw mesh so we can watch how it rotates
-        ball2.setTranslateX(100.0);
-        ballXForm.getChildren().add(ball);
-        ballXForm.getChildren().add(ball2);
+        ball2XForm.setLayoutX(50.0);
+        ball2XForm.setLayoutY(-50.0);
+        ball2XForm.getChildren().add(ball2);
+        ball3 = new Sphere(50);
+        ball3.setDrawMode(DrawMode.LINE); // draw mesh so we can watch how it rotates
+        ball3XForm.setLayoutX(-50.0);
+        ball3XForm.setLayoutY(50.0);
+        ball3XForm.getChildren().add(ball3);
+        ball4 = new Sphere(50);
+        ball4.setDrawMode(DrawMode.LINE); // draw mesh so we can watch how it rotates
+        ball4XForm.setLayoutX(50.0);
+        ball4XForm.setLayoutY(50.0);
+        ball4.setScaleX(-1);
+        ball4XForm.getChildren().add(ball4);
        root.getChildren().addAll(ballXForm);
+       root.getChildren().addAll(ball2XForm);
+       root.getChildren().addAll(ball3XForm);
+       root.getChildren().addAll(ball4XForm);
 
         Scene scene = new Scene(root, 1024, 768, true);
         scene.setFill(Color.GREY);
